4d4b219b54c18e2be9a8428600914d5b3a1a7886
useHTML5 <mne@sait-nado.ru>
2020-02-22 11:41:56 +0500

    new version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="text_ckeditor",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
